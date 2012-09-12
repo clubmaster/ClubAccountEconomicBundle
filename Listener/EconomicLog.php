cbc28118f482a53d57c99c082eb3fd5dfdc27917
Michael Holm <hollo@hollo.dk>
2012-09-12 11:43:13 +0200

    Bind to orderpaid event.

--- Listener/EconomicLog.php
+++ Listener/EconomicLog.php
@@ -11,17 +11,21 @@ class EconomicLog
     $this->container = $container;
   }
 
-  public function onPurchaseCreate(\Club\ShopBundle\Event\FilterPurchaseLogEvent $event)
+  public function onOrderPaid(\Club\ShopBundle\Event\FilterPurchaseLogEvent $event)
   {
     $purchase = $event->getPurchaseLog();
 
+    $c = new \Club\Account\EconomicBundle\Model\Config();
+    $c->url = $this->container->getParameter('club_account_economic.economic_url');
+    $c->agreementNumber = $this->container->getParameter('club_account_economic.agreement');
+    $c->userName = $this->container->getParameter('club_account_economic.username');
+    $c->password = $this->container->getParameter('club_account_economic.password');
+    $c->cashbook = $this->container->getParameter('club_account_economic.cashbook');
+    $c->currency = $this->container->getParameter('club_account_economic.currency');
+    $c->contraAccount = $this->container->getParameter('club_account_economic.contraAccount');
+
     $economic = $this->container->get('club_account_economic.economic');
-    $economic->setConfig(array(
-      'url' => $this->container->getParameter('club_account_economic.economic_url'),
-      'agreementNumber' => $this->container->getParameter('club_account_economic.agreement'),
-      'userName' => $this->container->getParameter('club_account_economic.username'),
-      'password' => $this->container->getParameter('club_account_economic.password')
-    ));
+    $economic->setConfig($c);
 
     foreach ($purchase->getOrder()->getOrderProducts() as $prod) {
       $economic->addFinanceVoucher($prod);
